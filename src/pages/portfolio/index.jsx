3099b6e6ddb9b97ff379d9cc7d5f6e7935e30973
assavlatbek <assavlatbek@gmail.com>
2023-12-22 15:16:53 +0500

    some animations added

---
+++ src/pages/portfolio/index.jsx
@@ -0,0 +1,15 @@
+import "./portfolio.css";
+
+const PortfolioPage = () => {
+  return (
+    <section id="portfolio">
+      <div className="container">
+        <h1 className="section-title" data-aos="flip-right">
+          Portfolios
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default PortfolioPage;
